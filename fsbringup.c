147f32d0b326dbccc1811e4b3e8cdf3f1e77e9ee
Ali Raza <aliraza@bu.edu>
2020-03-18 23:09:58 +0000

    adding fs code

--- fsbringup.c
+++ fsbringup.c
@@ -6,58 +6,82 @@
 #include <stdio.h>
 #include <sys/sysmacros.h>
 #include <stdlib.h>
-
-/*
-extern int printk(const char *fmt, ...);
-extern long ukl_open(const char* filename, int flags, unsigned short mode);
-extern long ukl_close(int fd);
-extern ssize_t ukl_write(int fd, const void* buf, size_t count);
-extern int ukl_mount(char * dev_name, char * dir_name, char * type, unsigned long flags, void * data);
-extern int ukl_chroot(const char * filename);
-extern int ukl_chdir(const char * filename);
-*/
+#include <dirent.h>
 
 int kmain(void){
-	mkdir ("/sys", 0755);
+
+	// Building and Mounting tmpfs
+
+	ukl_mkdir ("/mytmpfs", 0777);
+	ukl_mkdir ("/root", 0777);
+
+	if(ukl_mount("tmpfs", "/mytmpfs", "tmpfs", MS_MGC_VAL, "size=2g") == -1){
+		printk("mount: /mytmpfs fail");
+	} else {
+		printk("mount /mytmpfs successful!\n");
+	}
+
+	// Mounting root files system provided by QEMU
+
 	if (mknod ("/dev/root", S_IFBLK|0700, makedev (8,0)) == -1) {
 		printk("mknod: /dev/root");
 	} else {
 		printk("mknod successful!\n");
 	}
+	
 
-	if (ukl_mount ("/dev/root", "/root", "ext2", MS_NOATIME, "dax") == -1) {
+	if (ukl_mount ("/dev/root", "/root", "ext2", MS_NOATIME, "") == -1) {
 		printk("mount: /root");
 	} else {
 		printk("mount successful!\n");
 	}
 
-	if (ukl_chroot ("/root") == -1) {
-		printk ("chroot: /root");
-	} else {
-		printk("chroot successful!\n");
-	}
-	ukl_chdir ("/");
-
-	/*
-	int fp;
-
-	fp = ukl_open("/home/test.txt", O_WRONLY | O_APPEND, S_IRWXU);
-	ukl_write(fp, "This is testing for write...\n  ", 29);
-	ukl_close(fp);
-	*/
+	// Copying files from /dev/root/ukl to tmpfs
 
 	FILE *fptr;
-	
+	FILE *source, *target;
+	char ch;
+
 	// use appropriate location if you are using MacOS or Linux
-	fptr = fopen("/home/test.txt","w");
+	fptr = fopen("/mytmpfs/test.txt","w");
 	
 	if(fptr == NULL)
 	{
-		printk("Error!");
+		printk("Error while writing!\n");
 		exit(1);
 	}
 	
 	fprintf(fptr,"Testing the filesystem by writing to it\n");
 	fclose(fptr);
+	
+	source = fopen("/mytmpfs/test.txt", "r");
+
+	if( source == NULL )
+	{
+	   printk("source problem\n");
+	   exit(EXIT_FAILURE);
+	}
+
+	target = fopen("/root/target.txt", "w");
+
+	if( target == NULL )
+	{
+	   fclose(source);
+	   printf("target problem\n");
+	   exit(EXIT_FAILURE);
+	}
+
+	while( ( ch = fgetc(source) ) != EOF )
+	   fputc(ch, target);
+
+	printk("File copied successfully.\n");
+
+	fclose(source);
+	fclose(target);
+	
+	ukl_sync();
+
 	return 0;
+	
 }
+
